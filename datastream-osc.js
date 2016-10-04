5c8fb160b5fffc59005b776de6d912ae8ba31586
Stefan Dzisiewski-Smith <stefan@bareconductive.com>
2016-10-04 17:58:30 +0100

    Added readme, updated license, added comments and header comment block, added runner

--- datastream-osc.js
+++ datastream-osc.js
@@ -28,11 +28,12 @@ var osc    = require('omgosc');
 
 var mpr121;
 
+// default host and port
 var host = '127.0.0.1';
 var port = 3000;
 
 function printHelp() {
-  console.log('Sends Pi Cap readings through OSC - MUST be run as root.\n');
+  console.log('Streams Pi Cap data over OSC - MUST be run as root.\n');
   console.log('Usage: node datastream-osc.js [OPTIONS]\n');
   console.log('Options:');
   console.log('  -h, --host   host address, defaults to 127.0.0.1');
@@ -41,6 +42,8 @@ function printHelp() {
 
   process.exit(0);
 }
+
+// sift through the arguments and set stuff up / show help as appropriate
 
 if (argv.help) { printHelp(); }
 if (argv.h || argv.host) { host = argv.h || argv.host; }
@@ -58,18 +61,25 @@ var sender = new osc.UdpSender(host, parseInt(port));
 
 console.log('sending to: ' + host + ':' + port);
 
+
 mpr121.on('data', function(data) {
+  // split out each of the various data streams...
   var touch = data.map(function(electrode) { return electrode.isTouched ? 1 : 0; });
   var tths  = data.map(function(electrode) { return electrode.touchThreshold; });
   var rths  = data.map(function(electrode) { return electrode.releaseThreshold; });
   var fdat  = data.map(function(electrode) { return electrode.filtered; });
   var bval  = data.map(function(electrode) { return electrode.baseline; });
   var diff  = data.map(function(electrode) { return electrode.baseline - electrode.filtered; });
-
+  // ...and send them out via OSC - simples!
   sender.send('/touch', touch.map(function() { return 'i'; }).join(''), touch);
   sender.send('/tths',  tths.map(function() { return 'i'; }).join(''), tths);
   sender.send('/rths',  rths.map(function() { return 'i'; }).join(''), rths);
   sender.send('/fdat',  fdat.map(function() { return 'i'; }).join(''), fdat);
   sender.send('/bval',  bval.map(function() { return 'i'; }).join(''), bval);
   sender.send('/diff',  diff.map(function() { return 'i'; }).join(''), diff);
+});
+
+// this allows us to exit the program via Ctrl+C while still exiting elegantly
+process.on('SIGINT', function () {
+  process.exit(0);
 });
